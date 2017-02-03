004ab1da34d5821d45f6eef77df6ced0509bab83
Sarath Chandra J <sarath@peel.com>
2017-02-03 21:33:42 +0530

    asking for location, if turned off

--- www/js/app.js
+++ www/js/app.js
@@ -26,16 +26,16 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services',
       if (window.StatusBar) {
         StatusBar.styleDefault();
       }
-      function onRequestSuccess(success){
-        console.log("Successfully requested accuracy: "+success.message);
+      function onRequestSuccess(success) {
+        console.log("Successfully requested accuracy: " + success.message);
         GoogleMaps.init();
       }
 
-      function onRequestFailure(error){
-        console.error("Accuracy request failed: error code="+error.code+"; error message="+error.message);
-        if(error.code !== cordova.plugins.locationAccuracy.ERROR_USER_DISAGREED){
-          if(window.confirm("Failed to automatically set Location Mode to 'High Accuracy'. " +
-              "Would you like to switch to the Location Settings page and do this manually?")){
+      function onRequestFailure(error) {
+        console.error("Accuracy request failed: error code=" + error.code + "; error message=" + error.message);
+        if (error.code !== cordova.plugins.locationAccuracy.ERROR_USER_DISAGREED) {
+          if (window.confirm("Failed to automatically set Location Mode to 'High Accuracy'. " +
+              "Would you like to switch to the Location Settings page and do this manually?")) {
             cordova.plugins.diagnostic.switchToLocationSettings();
           }
         }
@@ -43,6 +43,17 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services',
 
       cordova.plugins.locationAccuracy.request(onRequestSuccess, onRequestFailure,
         cordova.plugins.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY);
+
+      cordova.plugins.diagnostic.registerLocationStateChangeHandler(function (state) {
+        if ((device.platform === "Android" && state !== cordova.plugins.diagnostic.locationMode.LOCATION_OFF)) {
+          console.log("Location is available");
+        } else {
+          cordova.plugins.locationAccuracy.request(onRequestSuccess, onRequestFailure,
+            cordova.plugins.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY);
+        }
+      });
+
+
     });
   })
 
